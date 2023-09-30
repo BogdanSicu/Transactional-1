161037c862e383ddd9459611483ad9c132595a33
Sicu Bogdan-Andrei <b0gdan.munt3@gmail.com>
2023-09-30 15:30:44 +0300

    Finished

--- transactional/src/main/java/munte/transactional/dto/EmployeeDTO.java
+++ transactional/src/main/java/munte/transactional/dto/EmployeeDTO.java
@@ -10,5 +10,5 @@ import lombok.NoArgsConstructor;
 public class EmployeeDTO {
     private String empName;
     private String email;
-    private Long departmentId;
+    private String departmentName;
 }
--- transactional/src/main/java/munte/transactional/service/EmployeeServiceImpl.java
+++ transactional/src/main/java/munte/transactional/service/EmployeeServiceImpl.java
@@ -1,10 +1,13 @@
 package munte.transactional.service;
 
 import munte.transactional.dto.EmployeeDTO;
+import munte.transactional.entity.Department;
+import munte.transactional.entity.Employee;
 import munte.transactional.repository.DepartmentRepository;
 import munte.transactional.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -20,7 +23,23 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
     @Override
+    @Transactional
     public String saveEmployee(EmployeeDTO employeeDTO) {
-        return null;
+
+        Department department = new Department();
+        department.setDepartmentName(employeeDTO.getDepartmentName());
+        department.setDepartmentCode(employeeDTO.getDepartmentName());
+
+        Long departmentId = departmentRepository.save(department).getDepartmentId();
+
+//        Employee employee = new Employee();
+        Employee employee = null;
+        employee.setEmpName(employeeDTO.getEmpName());
+        employee.setEmail(employeeDTO.getEmail());
+        employee.setDepartmentId(departmentId);
+
+        employeeRepository.save(employee);
+
+        return "Employee is saved with Employee ID : " + employee.getEmployeeId();
     }
 }
